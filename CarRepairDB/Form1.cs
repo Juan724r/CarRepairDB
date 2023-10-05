3d2d79f023f74e5614b2f681a1b6bb8f6ced6891
Nikita Serov <stud0000262775@study.utmn.ru>
2023-10-05 23:51:37 +0500

    added links

--- CarRepairDB/Form1.cs
+++ CarRepairDB/Form1.cs
@@ -19,91 +19,94 @@ namespace CarRepairDB
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            DataSet carWorkshop = new DataSet("Car Workshop");
+            DataSet carWorkshop = new DataSet("CarWorkshop");
 
-            DataTable workTypes = new DataTable("Work Types");
-            DataColumn workTypeID = new DataColumn("ID", typeof(int));
+            DataTable workTypesTable = new DataTable("WorkTypes");
+            DataColumn workTypeID = workTypesTable.Columns.Add("WorkTypeID", typeof(int));
             workTypeID.Unique = true;
             workTypeID.AllowDBNull = false;
             workTypeID.AutoIncrement = true;
             workTypeID.AutoIncrementSeed = 1;
             workTypeID.AutoIncrementStep = 1;
-            DataColumn workType = new DataColumn("Type", typeof(string));
-            workTypes.Columns.Add(workTypeID);
-            workTypes.Columns.Add(workType);
+            workTypesTable.Columns.Add("WorkType", typeof(string));
+            workTypesTable.PrimaryKey = new DataColumn[] { workTypeID };
+            carWorkshop.Tables.Add(workTypesTable);
 
-
-            DataTable carModels = new DataTable("Car Models");
-            DataColumn carModelID = new DataColumn("ID", typeof(int));
+            DataTable carModelsTable = new DataTable("CarModels");
+            DataColumn carModelID = carModelsTable.Columns.Add("CarModelID", typeof(int));
             carModelID.Unique = true;
             carModelID.AllowDBNull = false;
             carModelID.AutoIncrement = true;
             carModelID.AutoIncrementSeed = 1;
             carModelID.AutoIncrementStep = 1;
-            DataColumn carModel = new DataColumn("Model", typeof(string));
-            carModels.Columns.Add(carModelID);
-            carModels.Columns.Add(carModel);
+            carModelsTable.Columns.Add("CarModel", typeof(string));
+            carModelsTable.PrimaryKey = new DataColumn[] { carModelID };
+            carWorkshop.Tables.Add(carModelsTable);
 
-
-            DataTable carOwners = new DataTable("Car Owners");
-            DataColumn carOwnerID = new DataColumn("ID", typeof(int));
+            DataTable carOwnersTable = new DataTable("CarOwners");
+            DataColumn carOwnerID = carOwnersTable.Columns.Add("CarOwnerID", typeof(int));
             carOwnerID.Unique = true;
             carOwnerID.AllowDBNull = false;
             carOwnerID.AutoIncrement = true;
             carOwnerID.AutoIncrementSeed = 1;
             carOwnerID.AutoIncrementStep = 1;
-            DataColumn carOwnerName = new DataColumn("Name", typeof(string));
-            DataColumn carOwnerPassportSerial = new DataColumn("Passport Serial", typeof(string));
-            DataColumn carOwnerPassportNumber = new DataColumn("Passport Number", typeof(string));
-            DataColumn carOwnerPassportOther = new DataColumn("Passport Other", typeof(string));
-            carOwners.Columns.Add(carOwnerID);
-            carOwners.Columns.Add(carOwnerName);
-            carOwners.Columns.Add(carOwnerPassportSerial);
-            carOwners.Columns.Add(carOwnerPassportNumber);
-            carOwners.Columns.Add(carOwnerPassportOther);
+            carOwnersTable.Columns.Add("CarOwnerName", typeof(string));
+            carOwnersTable.Columns.Add("PassportSerial", typeof(string));
+            carOwnersTable.Columns.Add("PassportNumber", typeof(string));
+            carOwnersTable.Columns.Add("PassportIssuedBy", typeof(string));
+            carOwnersTable.Columns.Add("PassportIssueDate", typeof(DateTime));
+            carOwnersTable.PrimaryKey = new DataColumn[] { carOwnerID };
+            carWorkshop.Tables.Add(carOwnersTable);
 
-
-            DataTable cars = new DataTable("Cars");
-            DataColumn carID = new DataColumn("ID", typeof(int));
+            DataTable carsTable = new DataTable("Cars");
+            DataColumn carID = carsTable.Columns.Add("CarID", typeof(int));
             carID.Unique = true;
             carID.AllowDBNull = false;
             carID.AutoIncrement = true;
             carID.AutoIncrementSeed = 1;
             carID.AutoIncrementStep = 1;
-            DataColumn carLicensePlate = new DataColumn("License Plate", typeof(string));
-            cars.Columns.Add(carID);
-            cars.Columns.Add(carLicensePlate);
+            carsTable.Columns.Add("CarModelID", typeof(int));
+            carsTable.Columns.Add("CarOwnerID", typeof(int));
+            carsTable.Columns.Add("LicensePlate", typeof(string));
+            carsTable.PrimaryKey = new DataColumn[] { carID };
+            carWorkshop.Tables.Add(carsTable);
 
-
-            DataTable masters = new DataTable("Masters");
-            DataColumn masterID = new DataColumn("ID", typeof(int));
+            DataTable mastersTable = new DataTable("Masters");
+            DataColumn masterID = mastersTable.Columns.Add("MasterID", typeof(int));
             masterID.Unique = true;
             masterID.AllowDBNull = false;
             masterID.AutoIncrement = true;
             masterID.AutoIncrementSeed = 1;
             masterID.AutoIncrementStep = 1;
-            DataColumn masterName = new DataColumn();
-            DataColumn masterPassportSerial = new DataColumn("Passport Serial", typeof(string));
-            DataColumn masterPassportNumber = new DataColumn("Passport Number", typeof(string));
-            DataColumn masterPassportOther = new DataColumn("Passport Other", typeof(string));
-            DataColumn masterDateOfBirth = new DataColumn("Date of Birth", typeof(string));
-            masters.Columns.Add(masterID);
-            masters.Columns.Add(masterName);
-            masters.Columns.Add(masterPassportSerial);
-            masters.Columns.Add(masterPassportNumber);
-            masters.Columns.Add(masterPassportOther);
-            masters.Columns.Add(masterDateOfBirth);
-            
+            mastersTable.Columns.Add("MasterName", typeof(string));
+            mastersTable.Columns.Add("PassportSerial", typeof(string));
+            mastersTable.Columns.Add("PassportNumber", typeof(string));
+            mastersTable.Columns.Add("PassportIssuedBy", typeof(string));
+            mastersTable.Columns.Add("DateOfBirth", typeof(DateTime));
+            mastersTable.PrimaryKey = new DataColumn[] { masterID };
+            carWorkshop.Tables.Add(mastersTable);
 
-            DataTable works = new DataTable("Works");
+            DataTable worksTable = new DataTable("Works");
+            DataColumn workID = worksTable.Columns.Add("WorkID", typeof(int));
+            workID.Unique = true;
+            workID.AllowDBNull = false;
+            workID.AutoIncrement = true;
+            workID.AutoIncrementSeed = 1;
+            workID.AutoIncrementStep = 1;
+            worksTable.Columns.Add("CarID", typeof(int));
+            worksTable.Columns.Add("WorkTypeID", typeof(int));
+            worksTable.Columns.Add("WorkStartDate", typeof(DateTime));
+            worksTable.Columns.Add("WorkMasterID", typeof(int));
+            worksTable.Columns.Add("WorkEndDate", typeof(DateTime));
+            worksTable.Columns.Add("WorkDescription", typeof(string));
+            worksTable.PrimaryKey = new DataColumn[] { workID };
+            carWorkshop.Tables.Add(worksTable);
 
-
-            carWorkshop.Tables.Add(workTypes);
-            carWorkshop.Tables.Add(carModels);
-            carWorkshop.Tables.Add(carOwners);
-            carWorkshop.Tables.Add(cars);
-            carWorkshop.Tables.Add(masters);
-            carWorkshop.Tables.Add(works);
+            carWorkshop.Relations.Add("Cars_CarModels", carModelsTable.Columns["CarModelID"], carsTable.Columns["CarModelID"]);
+            carWorkshop.Relations.Add("Cars_CarOwners", carOwnersTable.Columns["CarOwnerID"], carsTable.Columns["CarOwnerID"]);
+            carWorkshop.Relations.Add("Works_Cars", carsTable.Columns["CarID"], worksTable.Columns["CarID"]);
+            carWorkshop.Relations.Add("Works_WorkTypes", workTypesTable.Columns["WorkTypeID"], worksTable.Columns["WorkTypeID"]);
+            carWorkshop.Relations.Add("Works_Masters", mastersTable.Columns["MasterID"], worksTable.Columns["WorkMasterID"]);
         }
     }
 }
